9bc4c3138dac66ee45eab56b3a80c7ba6afa5a33
Илья С <skachilya@gmail.com>
2024-07-03 23:56:25 +0300

    ver 0.1

--- src/components/Count/Count.jsx
+++ src/components/Count/Count.jsx
@@ -2,13 +2,19 @@ import React, { useState, useEffect, useRef } from "react";
 import "./Count.css";
 
 const Count = () => {
-  const [count, setCount] = useState(1000000000);
-  const intervalId = useRef(null); // Используем useRef для сохранения ID интервала
+  // Пытаемся получить начальное значение счетчика из localStorage, иначе используем 1000000000
+  const initialCount = Number(localStorage.getItem("count")) || 1000000000;
+  const [count, setCount] = useState(initialCount);
+  const intervalId = useRef(null);
 
   useEffect(() => {
     // Устанавливаем начальный интервал уменьшения на 3 в секунду
     intervalId.current = setInterval(() => {
-      setCount((prevCount) => prevCount - 3);
+      setCount((prevCount) => {
+        const newCount = prevCount - 3;
+        localStorage.setItem("count", newCount.toString()); // Сохраняем новое значение в localStorage
+        return newCount;
+      });
     }, 1000);
 
     return () => clearInterval(intervalId.current); // Очистка интервала при размонтировании
@@ -17,21 +23,26 @@ const Count = () => {
   const startCounting = (decrementValue, interval) => {
     clearInterval(intervalId.current); // Очищаем текущий интервал
     intervalId.current = setInterval(() => {
-      setCount((prevCount) => prevCount - decrementValue);
+      setCount((prevCount) => {
+        const newCount = prevCount - decrementValue;
+        localStorage.setItem("count", newCount.toString()); // Сохраняем новое значение в localStorage
+        return newCount;
+      });
     }, interval);
   };
 
   const handleInteractionStart = () => {
-    startCounting(1, 1000); // Уменьшаем на 1 каждые две секунды
+    startCounting(1, 1000); // Уменьшаем на 1 каждую секунду
   };
 
   const handleInteractionEnd = () => {
-    startCounting(3, 1000); // Возвращаемся к уменьшению на 3 каждую секунду
+    startCounting(2, 1000); // Возвращаемся к уменьшению на 3 каждую секунду
   };
 
   return (
     <>
       <section className="count">
+				<div className="count__title">Версия 0.1</div>
         <div className="count__sum">{count}</div>
         <button
           onMouseDown={handleInteractionStart}
